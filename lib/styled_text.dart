daa01d8987de1884204fa9afda29c6de26458504
ambi hidalgo <ambio.pk@gmail.com>
2023-08-18 15:16:54 -0700

    Create dicerolling application

---
+++ lib/styled_text.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class StyledText extends StatelessWidget {
+  const StyledText(this.text, {super.key});
+
+  final String text;
+
+  @override
+  Widget build(context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        color: Color.fromARGB(255, 137, 201, 94),
+        fontSize: 68,
+      ),
+    );
+  }
+}
